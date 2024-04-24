69de7b7233dced02aa267eafabc3cc58d3621266
Kanzeer <miguelroaamaral@gmail.com>
2024-04-24 16:28:34 -0400

    exemplo de arrow function

---
+++ quadrado.js
@@ -0,0 +1,10 @@
+const quadrado = (x) => {
+    return x * x;
+};
+console.log(quadrado(89)); //saída 7921
+
+const quadradoDois = (x) => x * x;
+console.log(quadradoDois(6)); //saída 36
+
+const quadradoTres = x => x * x;
+console.log(quadradoTres(55)); //saída 3025
