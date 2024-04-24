f96210d310c16f9edb865b9b9b88bec234404d4f
Kanzeer <miguelroaamaral@gmail.com>
2024-04-24 16:27:15 -0400

    questão 2 finalizada

---
+++ questao2.js
@@ -0,0 +1,6 @@
+//Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome como parâmetro e retorna uma string de saudação.
+
+const saudacao = (nome) => {
+    return "Olá " + nome + "!" ;
+};
+console.log(saudacao("Miguel"));
