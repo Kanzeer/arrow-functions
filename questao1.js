4e376e36e7419a2415b2762b54b6883b3cb4ef43
Kanzeer <miguelroaamaral@gmail.com>
2024-04-24 16:27:03 -0400

    questão 1 finalizada

---
+++ questao1.js
@@ -0,0 +1,6 @@
+//Questão 01: Escreva uma arrow function chamada dobro que recebe um número como parâmetro e retorna o dobro desse número.
+
+const quadrado = (x) => {
+    return x * 2;
+};
+console.log(quadrado(2)); //saída 4
